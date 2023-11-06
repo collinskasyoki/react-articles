a8c575659de2263d447f2045ae2e23797539c326
Collins Kasyoki <collinskasyoki@gmail.com>
2023-11-06 21:57:09 +0300

    Fix site page layout

--- src/app/pages/sites.jsx
+++ src/app/pages/sites.jsx
@@ -6,29 +6,31 @@ const Sites = () => {
   const { data: sites, status } = useQuery("sites", () => fetchSites());
 
   return (
-    <div className="row">
-      <div className="col-twelve">
-        {status === "error" && <p>Error fetching sites!</p>}
-        {status === "loading" && <p>Error fetching sites!</p>}
-        {status === "success" && Array.isArray(sites) ? (
-          <>
-            {sites.map((site) => {
-              return (
-                <>
-                  <div className="primary-content" key={site.id}>
-                    <h1 className="entry-title add-bottom">{site.title}</h1>
-                    <p className="lead">{site.description}</p>
+    <section id="content-wrap" class="site-page">
+      <div className="bricks row">
+        <div className="col-twelve">
+          {status === "error" && <p>Error fetching sites!</p>}
+          {status === "loading" && <p>Error fetching sites!</p>}
+          {status === "success" && Array.isArray(sites) ? (
+            <>
+              {sites.map((site) => {
+                return (
+                  <div key={site.id}>
+                    <div className="primary-content">
+                      <h1 className="entry-title add-bottom">{site.title}</h1>
+                      <p className="lead">{site.description}</p>
+                    </div>
+                    <hr />
                   </div>
-                  <hr />
-                </>
-              );
-            })}
-          </>
-        ) : (
-          ""
-        )}
+                );
+              })}
+            </>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
