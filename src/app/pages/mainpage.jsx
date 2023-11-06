d1d67958509ec14df5d113d1adefc423f1338f3d
Collins Kasyoki <collinskasyoki@gmail.com>
2023-11-06 16:51:26 +0300

    Separate article card rendering to its own component

--- src/app/pages/mainpage.jsx
+++ src/app/pages/mainpage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { Link } from "react-router-dom";
 import { fetchArticles } from "./data";
+import ArticleRenderer from "./helpers/ArticleRenderer";
 
 const MainPage = () => {
   const { data: articles, status } = useQuery("articles", () =>
@@ -34,43 +34,7 @@ const MainPage = () => {
                   </blockquote>
                 </div>
               </article>
-              {articles && Array.isArray(articles.items)
-                ? articles.items.map((onearticle) => {
-                    return (
-                      <article
-                        key={onearticle.id}
-                        className="brick entry format-standard animate-this"
-                      >
-                        <div className="entry-thumb">
-                          <Link
-                            to={`/item/${onearticle.id}`}
-                            className="thumb-link"
-                          >
-                            <img
-                              src={onearticle.image_thumbnail_url}
-                              alt="image"
-                            />
-                          </Link>
-                        </div>
-                        <div className="entry-text">
-                          <div className="entry-header">
-                            <div className="entry-meta">
-                              <span className="cat-links">
-                                <a href="#">Category 1</a>
-                                <a href="#">Category 2</a>
-                              </span>
-                            </div>
-                            <h1 className="entry-title">
-                              <Link to={`/item/${onearticle.id}`}>
-                                {onearticle.title}
-                              </Link>
-                            </h1>
-                          </div>
-                        </div>
-                      </article>
-                    );
-                  })
-                : "there"}
+              <ArticleRenderer articles={articles} />
             </div>
           </div>
 
