cd3031639a102a52926ca004c1bb1b63397212d4
Collins Kasyoki <collinskasyoki@gmail.com>
2023-11-06 12:18:19 +0300

    Enhancements

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,61 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
-          <div className="footer-main">
-            <div className="row">
-              <div className="col-eight tab-full mob-full footer-info">
-                <h4>About This Site</h4>
+      <div className="footer-main">
+        <div className="row">
+          <div className="col-eight tab-full mob-full footer-info">
+            <h4>About This Site</h4>
 
-                <p>
-                  Lorem ipsum Ut velit dolor Ut labore id fugiat in ut fugiat
-                  nostrud qui in dolore commodo eu magna Duis cillum dolor
-                  officia esse mollit proident Excepteur exercitation nulla.
-                  Lorem ipsum In reprehenderit commodo aliqua irure labore.
-                </p>
-              </div>
-
-              <div className="col-four tab-1-3 mob-1-2 site-links">
-                <h4>Site Links</h4>
-
-                <ul>
-                  <li>
-                    <Link to="/about">About</Link>
-                  </li>
-                  <li>
-                    <Link to="/categories">Categories</Link>
-                  </li>
-                  <li>
-                    <Link to="/sites">Sites</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <p>
+              Lorem ipsum Ut velit dolor Ut labore id fugiat in ut fugiat
+              nostrud qui in dolore commodo eu magna Duis cillum dolor officia
+              esse mollit proident Excepteur exercitation nulla. Lorem ipsum In
+              reprehenderit commodo aliqua irure labore.
+            </p>
           </div>
 
-          <div className="footer-bottom">
-            <div className="row">
-              <div className="col-twelve">
-                <div className="copyright">
-                  <span>© Copyright</span>
-                  <span>
-                    Design by{" "}
-                    <a href="http://www.styleshout.com/">styleshout</a>
-                  </span>
-                </div>
+          <div className="col-four tab-1-3 mob-1-2 site-links">
+            <h4>Site Links</h4>
 
-                <div id="go-top">
-                  <a className="smoothscroll" title="Back to Top" href="#top">
-                    <i className="icon icon-arrow-up"></i>
-                  </a>
-                </div>
-              </div>
+            <ul>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/categories">Categories</Link>
+              </li>
+              <li>
+                <Link to="/sites">Sites</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="row">
+          <div className="col-twelve">
+            <div className="copyright">
+              <span>© Copyright</span>
+              <span>
+                Design by <a href="http://www.styleshout.com/">styleshout</a>
+              </span>
+            </div>
+
+            <div id="go-top">
+              <a className="smoothscroll" title="Back to Top" href="#top">
+                <i className="icon icon-arrow-up"></i>
+              </a>
             </div>
           </div>
-        </footer>
-  )
-}
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,96 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="short-header">
-          <div className="gradient-block"></div>
+      <div className="gradient-block"></div>
 
-          <div className="row header-content">
-            <div className="logo">
-              <Link to="/">Author</Link>
-            </div>
+      <div className="row header-content">
+        <div className="logo">
+          <Link to="/">Author</Link>
+        </div>
 
-            <nav id="main-nav-wrap">
-              <ul className="main-navigation sf-menu">
-                <li><Link to="/">Home</Link></li>
-                {/* <li className="current">
-                  <a href="index.html" title="">
-                    Home
-                  </a>
-                </li> */}
-                <li>
-                  <Link to="/categories">Categories</Link>
-                </li>
-                {/* <li className="has-children">
-                  <a href="#" title="">
-                    Categories
-                  </a>
-                  <ul className="sub-menu">
-                    <li>
-                      <a href="#">Wordpress</a>
-                    </li>
-                    <li>
-                      <a href="#">Sites</a>
-                    </li>
-                    <li>
-                      <a href="#">Photography</a>
-                    </li>
-                  </ul>
-                </li> */}
-                <li>
-                  <Link to="/sites">Sites</Link>
-                </li>
-                
-              </ul>
-            </nav>
+        <nav id="main-nav-wrap">
+          <ul className="main-navigation sf-menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+            <li>
+              <Link to="/sites">Sites</Link>
+            </li>
+          </ul>
+        </nav>
 
-            <div className="search-wrap">
-              <form
-                role="search"
-                method="get"
-                className="search-form"
-                action="#"
-              >
-                <label>
-                  <span className="hide-content">Search for:</span>
-                  <input
-                    // Remove this when implementing
-                    readOnly={true}
-                    type="search"
-                    className="search-field"
-                    placeholder="Type Your Keywords"
-                    value=""
-                    name="s"
-                    title="Search for:"
-                    autoComplete="off"
-                  />
-                </label>
-                {/* Remove readonly when implementing */}
-                <input
-                  readOnly={true}
-                  type="submit"
-                  className="search-submit"
-                  value="Search"
-                />
-              </form>
+        <div className="search-wrap">
+          <form role="search" method="get" className="search-form" action="#">
+            <label>
+              <span className="hide-content">Search for:</span>
+              <input
+                // Remove this when implementing
+                readOnly={true}
+                type="search"
+                className="search-field"
+                placeholder="Type Your Keywords"
+                value=""
+                name="s"
+                title="Search for:"
+                autoComplete="off"
+              />
+            </label>
+            {/* Remove readonly when implementing */}
+            <input
+              readOnly={true}
+              type="submit"
+              className="search-submit"
+              value="Search"
+            />
+          </form>
 
-              <a href="#" id="close-search" className="close-btn">
-                Close
-              </a>
-            </div>
+          <a href="#" id="close-search" className="close-btn">
+            Close
+          </a>
+        </div>
 
-            <div className="triggers">
-              <a className="search-trigger" href="#">
-                <i className="fa fa-search"></i>
-              </a>
-              <a className="menu-toggle" href="#">
-                <span>Menu</span>
-              </a>
-            </div>
-          </div>
-        </header>
-  )
-}
+        <div className="triggers">
+          <a className="search-trigger" href="#">
+            <i className="fa fa-search"></i>
+          </a>
+          <a className="menu-toggle" href="#">
+            <span>Menu</span>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
